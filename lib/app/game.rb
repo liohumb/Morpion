16e7504dc6db9c7a8162bb93607746fbea249856
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 15:05:21 +0200

    perform

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -16,6 +16,60 @@ class Game
     @board_game_instance = Board.new
   end
 
-  
+  def perform
+
+    while @board_game_instance.game_finished? == false && @board_game_instance.there—is—a—winner == false
+      if @num_round == 0
+        puts "• Appuie sur ENTRER pour commencer •"
+        puts gets
+      end
+
+      @board_game_instance.board_to_show
+
+      if @num_round%2==0 then
+        puts "C'est à #{@player_1.name} de jouer"
+        @board_game_instance.board_menu
+        choosen_box = gets.chomp.to_i
+        while @board_game_instance.check_player_move(choosen_box) == false
+          if choosen_box.between?(1,9)
+            puts "Tu ne peux pas jouer cette case, elle a déjà etait joué.. Choisis en une autre".yellow
+          else
+            puts "Hm.. Je te rappelle que tu dois jouer en choisissant un nombre entre 1 et 9".red
+          end
+          print " •  "
+          choosen_box = gets.chomp.to_i
+        end
+        @board_game_instance.game_move("X", choosen_box)
+      else
+        puts "C'est à #{@player_2.name} de jouer"
+        @board_game_instance.board_menu
+        choosen_box = gets.chomp.to_i
+        while @board_game_instance.check_player_move(choosen_box) == false
+          if choosen_box.between?(1,9)
+            puts "Tu ne peux pas jouer cette case, elle a déjà etait joué.. Choisis en une autre".yellow
+          else
+            puts "Hm.. Je te rappelle que tu dois jouer en choisissant un nombre entre 1 et 9".red
+          end
+          print " •  "
+          choosen_box = gets.chomp.to_i
+        end
+        @board_game_instance.game_move("O", choosen_box)
+      end
+
+      @num_round += 1
+
+    end
+
+    system("clear")
+
+    if @board_game_instance.game_finished? == true
+      @board_game_instance.board_show_draw
+    else
+      @board_game_instance.board_who_wins(@num_round, @player_1, @player_2)
+    end
+
+    @board_game_instance.show_result(@player_1, @player_2)
+
+  end
 
 end
