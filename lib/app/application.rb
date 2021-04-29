b38eb3a2927b76cee5499c545ed1897a60a91b22
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 17:09:24 +0200

    new players

--- lib/app/application.rb
+++ lib/app/application.rb
@@ -33,4 +33,47 @@ class Application
 
   end
 
+  def new_players
+
+    system("clear")
+    welcome
+    puts "Pour commencer, entre ton nom :"
+    print " •  "
+
+    player_1 = gets.chomp
+
+    while player_1 == ""
+      puts "Sois pas timide ! Dis moi ton petit nom :"
+      print " •  "
+      player_1 = gets.chomp
+    end
+
+    @player_1 = Player.new(player_1, "X")
+
+    system("clear")
+
+    puts "OK #{@player_1.name}, tu joues avec les 'X'"
+    puts "Ensuite, entre le nom du deuxième joueur :"
+    print " •  "
+
+    player_2 = gets.chomp
+
+    while player_2 == ""
+      puts "Sois pas timide ! Dis moi son petit nom :"
+      print " •  "
+      player_2 = gets.chomp
+    end
+
+    @player_2 = Player.new(player_2, "O")
+
+    system("clear")
+
+    puts "OK #{@player_2.name}, tu joues avec les '0'"
+    print "OK ? Appuie ENTRER quand tu es prêt(e)"
+    gets
+
+    system("clear")
+
+  end
+
 end
