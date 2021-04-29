5f148ad0123e7c1163bf29b6c5158d04c040a091
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 12:58:48 +0200

    class Game

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -4,3 +4,11 @@ $:.unshift File.expand_path('./../lib', __FILE__)
 require 'view/board'
 require 'app/player'
 require 'view/boardcase'
+
+class Game
+
+  attr_accessor :num_round, :player_1, :player_2, :board_game_instance
+
+  
+
+end
