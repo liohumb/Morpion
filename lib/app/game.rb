f92d331c1246b3052f44c6408a1f9990e40cd11a
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 14:38:52 +0200

    initialize

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -9,6 +9,13 @@ class Game
 
   attr_accessor :num_round, :player_1, :player_2, :board_game_instance
 
+  def initialize(first_player, second_player)
+    @num_round = 0
+    @player_1 = first_player
+    @player_2 = second_player
+    @board_game_instance = Board.new
+  end
+
   
 
 end
