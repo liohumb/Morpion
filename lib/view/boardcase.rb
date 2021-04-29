d621e19afa8c78136bcebc7771583b7e042aa0c0
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 16:08:52 +0200

    initialize

--- lib/view/boardcase.rb
+++ lib/view/boardcase.rb
@@ -3,5 +3,12 @@ Bundler.require
 $:.unshift File.expand_path('./../lib', __FILE__)
 
 class BoardCase
+
   attr_accessor :box_name, :box_value
+
+  def initialize(name)
+    @box_name = name
+    @box_value = ""
+  end
+
 end
