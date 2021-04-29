6cd681e16b535875114d7aefb7bed11af72254eb
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 17:29:02 +0200

    live menu

--- lib/view/show.rb
+++ lib/view/show.rb
@@ -13,4 +13,21 @@ class Show
     @board_case_array = board_game_instance
   end
 
+  def live_menu
+
+    puts "Choisis une case (disponible)"
+
+    num_box = 1
+
+    @board_case_array.each do |box|
+      if box.box_value == ""
+        puts "#{num_box} - #{box.box_name}"
+      end
+      num_box += 1
+    end
+
+    print " •  "
+
+  end
+
 end
