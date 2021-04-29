7f31cb0f313532e3fff9afeabfa76758b9ac786f
Lionel Humbert <helloliohumb@gmail.com>
2021-04-29 15:22:11 +0200

    class Board

--- lib/view/board.rb
+++ lib/view/board.rb
@@ -4,3 +4,10 @@ $:.unshift File.expand_path('./../lib', __FILE__)
 require 'view/boardcase'
 require 'view/show'
 require 'app/player'
+
+class Board
+
+  attr_accessor :board_case_name_array, :num_round, :show_instance
+  @@all_board = Array.new
+
+end
